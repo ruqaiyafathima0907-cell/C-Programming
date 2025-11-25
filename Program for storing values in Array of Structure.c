82a592f45c74a097db0a5e488ef67548d19678fe
ruqaiyafathima0907-cell <ruqaiyafathima0907@gmail.com>
2025-11-25 10:32:00 +0530

    Add files via upload

---
+++ Program for storing values in Array of Structure.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct student
+{
+int id;
+char name[20];
+};
+
+int main()
+{
+struct student s[2]={{101,"Anu"},{102,"Vasu"}};
+for(int i=0;i<2;i++)
+    printf("ID %d, Name : %s\n",s[i].id,s[i].name);
+    return 0;
+}
---
+++ Program to access Structure member using Pointer.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct student
+{
+int id;
+char name[20];
+};
+
+int main()
+{
+    struct student s={101,"Anu"};
+    struct student *ptr=&s;
+    printf("ID: %d\n",ptr->id);
+    printf("Name: %s\n",ptr->name);
+    return 0;
+}
