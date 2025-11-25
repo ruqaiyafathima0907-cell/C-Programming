326c2572b58de371f3368dd53ebf270d5250efa4
ruqaiyafathima0907-cell <ruqaiyafathima0907@gmail.com>
2025-11-25 10:45:22 +0530

    Add files via upload

---
+++ Program to pass Sturcture to Function using and pass by reference.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct student
+{
+int id;
+char name[20];
+};
+void display(struct student *s)
+{
+printf("ID: %d\n",s->id);
+printf("Name: %s\n ",s->name);
+}
+int main()
+{
+struct student s1={101,"Anu"};
+display(&s1);
+return 0;
+}
