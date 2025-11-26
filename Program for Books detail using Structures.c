1f8d492509d6586d8077d53da9a759c9327956fe
ruqaiyafathima0907-cell <ruqaiyafathima0907@gmail.com>
2025-11-26 13:53:48 +0530

    Update Program for Books detail using Structures.c

--- Program for Books detail using Structures.c
+++ Program for Books detail using Structures.c
@@ -10,16 +10,17 @@ float price;
 int main()
 {
 struct books b1={"The lost one","James",450.00};
-printf("%s %s %2f",b1.title,b1.author,b1.price);
+printf("%s\n %s\n %2f\n",b1.title,b1.author,b1.price);
 struct books b2={"Magical life","Diana",650.00};
-printf("%s %s %2f",b2.title,b2.author,b2.price);
+printf("%s\n %s\n %2f\n",b2.title,b2.author,b2.price);
 struct books b3={"Success isn't easy","Harry",1050.00};
-printf("%s %s %2f",b3.title,b3.author,b3.price);
+printf("%s\n %s\n %2f\n",b3.title,b3.author,b3.price);
 struct books b4={"Burning chains","Maya",1000.00};
-printf("%s %s %2f",b4.title,b4.author,b4.price);
+printf("%s\n %s\n %2f\n",b4.title,b4.author,b4.price);
 struct books b5={"Broken wings","Shanaya",1050.00};
-printf("%s %s %2f",b5.title,b5.author,b5.price);
+printf("%s\n %s\n %2f\n",b5.title,b5.author,b5.price);
   return 0;
 }
+
 
 
