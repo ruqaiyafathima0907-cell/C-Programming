7b4b781d0634f984b3907755175cd9601dd250c0
ruqaiyafathima0907-cell <ruqaiyafathima0907@gmail.com>
2025-11-26 11:11:19 +0530

    Add files via upload

---
+++ Program for Structure function for displaying student details.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct student {
+    int age;
+    char name[20];
+    float marks;
+};
+int main()
+ {
+    struct student s1 = {18, "Anu", 89.24};
+    printf("name: %s\n", s1.name);
+    printf("age: %d\n", s1.age);
+    printf("marks: %.2f\n", s1.marks);
+
+    struct student s2 = {20, "Maya", 96.5};
+    printf("name: %s\n", s2.name);
+    printf("age: %d\n", s2.age);
+    printf("marks: %.2f\n", s2.marks);
+
+    float avg = (s1.marks + s2.marks) / 2;
+    printf("Average: %.2f\n", avg);
+
+    return 0;
+}
